74450d56508671ac8aebcbf88ef93ef4df0eaa71
Syiam Fortunado <syiamfortunado@gmail.com>
2025-12-08 18:50:56 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
       reason: document.querySelector('#reason').value
     };
 
-    fetch('https://senticstrange.app.n8n.cloud/webhook/volunteer-form', {
+    fetch('https://syiamfortunado.app.n8n.cloud/webhook/volunteer-form', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data)
