df66ff638388d1ddafb5123c0ece73eda9ec3347
Syiam Fortunado <syiamfortunado@gmail.com>
2026-01-20 01:43:32 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -8,6 +8,6 @@ export const CLOUDFLARE_TUNNEL_BASE = 'https://clarke-bolt-feels-architects.tryc
 
 // Export all the full endpoints needed across your application
 export const N8N_FETCH_JOBS_URL = `${CLOUDFLARE_TUNNEL_BASE}/webhook/get-available-jobs`;
-export const N8N_SUBMIT_FORM_URL = `${CLOUDFLARE_TUNNEL_BASE}/webhook/volunteer-form`;
+export const N8N_SUBMIT_FORM_URL = `${CLOUDFLARE_TUNNEL_BASE}/webhook-test/volunteer-form`;
 export const N8N_TIME_IN_URL = `${CLOUDFLARE_TUNNEL_BASE}/webhook/time-in`;
 // Add any other endpoints here...
